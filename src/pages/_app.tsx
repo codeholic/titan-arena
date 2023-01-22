260df6eb72f73f413962fd02a72e6b995b756600
Ivan Fomichev <ivan.fomichev@eventinc.de>
2023-01-22 02:55:11 +0100

    create page layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,9 +9,20 @@ const WalletConnectionProvider = dynamic(() => import('../components/WalletConne
 });
 
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+import { CssBaseline, GlobalStyles } from '@mui/material';
+
+import '@fontsource/teko/400.css';
 
 export const ColorModeContext = createContext({ toggleColorMode: () => {} });
+
+declare module '@mui/material/styles/createTypography' {
+    interface TypographyOptions {
+        decorative: TypographyOptions['fontFamily'];
+    }
+    interface Typography {
+        decorative: Typography['fontFamily'];
+    }
+}
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
     const [mode, setMode] = useState('dark');
@@ -39,10 +50,13 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
 
         palette.mode = mode;
 
+        const joinFonts = (fonts: string[]) =>
+            fonts.map((font) => (font.indexOf(' ') < 0 ? font : `"${font}"`)).join(',');
+
         return createTheme({
             palette,
             typography: {
-                fontFamily: [
+                fontFamily: joinFonts([
                     '-apple-system',
                     'BlinkMacSystemFont',
                     'Segoe UI',
@@ -52,7 +66,23 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
                     'Cantarell',
                     'Helvetica Neue',
                     'sans-serif',
-                ].join(','),
+                ]),
+                button: {
+                    fontFamily: joinFonts(['Teko', 'sans-serif']),
+                    textTransform: 'uppercase',
+                    fontSize: '20px',
+                    letterSpacing: 2,
+                },
+            },
+            components: {
+                MuiLink: {
+                    styleOverrides: {
+                        root: {
+                            textDecoration: 'none',
+                            color: palette.text.primary,
+                        },
+                    },
+                },
             },
         });
     }, [mode]);
@@ -65,6 +95,17 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
         <ColorModeContext.Provider value={colorMode}>
             <ThemeProvider theme={theme}>
                 <CssBaseline />
+                <GlobalStyles
+                    styles={{
+                        body: {
+                            backgroundColor: 'rgb(16, 16, 16)',
+                            backgroundImage: 'url(/i/banner.png)',
+                            backgroundRepeat: 'no-repeat',
+                            backgroundSize: '100%',
+                            backgroundPosition: '50% -50px',
+                        },
+                    }}
+                />
                 <Toaster position="bottom-left" toastOptions={toastOptions} />
                 <WalletConnectionProvider>
                     <Component {...pageProps} />
