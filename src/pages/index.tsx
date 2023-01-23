a653293bc55472dcdcaa4d322760c4052c7ae620
Ivan Fomichev <ivan.fomichev@eventinc.de>
2023-01-23 20:50:20 +0100

    display owned NFTs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,33 @@ import React from 'react';
 
 import useSWR from 'swr';
 
-import { CardMedia, Container, Grid } from '@mui/material';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import { TOKEN_PROGRAM_ID } from '@solana/spl-token';
+
+import { CardMedia, Container, Grid, Skeleton } from '@mui/material';
 import { Navbar } from '../components/Navbar';
 
 const Home: NextPage = () => {
-    const fetcher = (url: string) => fetch(url).then((res) => res.json());
-    const { data, error, isLoading } = useSWR('/api/nfts', fetcher);
+    const wallet = useWallet();
+    const { connection } = useConnection();
 
-    console.log(data, error, isLoading);
+    const fetcher = (url: string) => {
+        if (!wallet.publicKey) {
+            return;
+        }
+
+        return fetch(url).then((res) =>
+            connection
+                .getParsedTokenAccountsByOwner(wallet.publicKey!, { programId: TOKEN_PROGRAM_ID })
+                .then(({ value }) => {
+                    const mints = value.reduce((result, tokenAccount) => ({ ...result, [tokenAccount.account.data.parsed.info.mint]: true }), {});
+
+                    return res.json().then(data => data.filter((nft: any) => mints[nft.mint]));
+                })
+        );
+    };
+
+    const { data, isLoading } = useSWR('/api/nfts', fetcher);
 
     return (
         <>
@@ -32,12 +51,20 @@ const Home: NextPage = () => {
                 <Navbar />
 
                 <Grid my={1} container spacing={2} columns={{ xs: 2, sm: 4, md: 6, lg: 8 }}>
-                    {data &&
-                        data.slice(0, 20).map(({ image_url }: any, index: number) => (
-                            <Grid item key={index} xs={1}>
-                                <CardMedia component="img" image={image_url} sx={{ borderRadius: '5px' }} />
-                            </Grid>
-                        ))}
+                    {!isLoading
+                        ? data &&
+                          data.map(({ image_url }: any, index: number) => (
+                              <Grid item key={index} xs={1}>
+                                  <CardMedia component="img" image={image_url} sx={{ borderRadius: '5px' }} />
+                              </Grid>
+                          ))
+                        : Array(16)
+                              .fill()
+                              .map((_, index) => (
+                                  <Grid item key={index} xs={1}>
+                                      <Skeleton key={index} variant="rounded" sx={{ paddingTop: '100%' }} />
+                                  </Grid>
+                              ))}
                 </Grid>
             </Container>
         </>
