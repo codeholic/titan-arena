27d7b4c614b28c0d9f918fe67c7a2722e5d0798c
Ivan Fomichev <ivan.fomichev@eventinc.de>
2023-01-24 04:39:36 +0100

    display two rows of skeletons

--- src/components/NftCardList.tsx
+++ src/components/NftCardList.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid } from '@mui/material';
+import { Box, Breakpoint, Grid, useMediaQuery, useTheme } from '@mui/material';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { useContext } from 'react';
 import { Nft } from '../hooks/useNfts';
@@ -7,16 +7,26 @@ import { NftCard } from './NftCard';
 
 export const NftCardList = () => {
     const wallet = useWallet();
+    const theme = useTheme();
 
     const { nfts, isLoading } = useContext(NftsContext);
+
+    const screen =
+        ['lg', 'md', 'sm'].reduce(
+            (result: string | undefined, screen) =>
+                useMediaQuery(theme.breakpoints.up(screen as Breakpoint)) ? result || screen : result,
+            undefined
+        ) || 'xs';
+
+    const columns = { xs: 2, sm: 4, md: 6, lg: 8 }[screen];
 
     return !wallet.connected ? (
         <Box my={2}>Connect your wallet to view baby titans.</Box>
     ) : !isLoading && nfts?.length === 0 ? (
         <Box my={2}>No baby titans owned.</Box>
     ) : (
-        <Grid my={1} container spacing={2} columns={{ xs: 2, sm: 4, md: 6, lg: 8 }}>
-            {(isLoading || !nfts ? Array(8).fill(undefined) : nfts).map((nft: Nft, index: number) => (
+        <Grid my={1} container spacing={2} columns={columns}>
+            {(isLoading || !nfts ? Array(columns * 2).fill(undefined) : nfts).map((nft: Nft, index: number) => (
                 <Grid item key={index} xs={1}>
                     <NftCard nft={nft} />
                 </Grid>
