b0d640571bc599e90e9a6fce18dbd90ae6d60551
Ivan Fomichev <ivan.fomichev@eventinc.de>
2023-02-03 05:03:11 +0100

    get current game stats API

--- src/pages/api/getCurrentGame.ts
+++ src/pages/api/getCurrentGame.ts
@@ -1,14 +1,65 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+import handleJsonResponse, { HandlerArgs } from '../../lib/handleJsonResponse';
 
-import { getCurrentGame } from '../../lib/queries';
-import { Game } from '../../lib/types';
-
-export type GetCurrentGameResult = {
-    currentGame: Game;
+type Stats = {
+    clanName?: string;
+    clanMultipler?: number;
+    total: number;
+    played: number;
+    points: number;
 };
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<GetCurrentGameResult | Error>) {
-    const { data: currentGame } = await getCurrentGame();
+type GetCurrentGameResult = {
+    clanStats: Stats[];
+    endsAt: Date;
+    opensAt: Date;
+    startsAt: Date;
+};
 
-    res.status(200).json({ currentGame });
-}
+const handler = async ({ req, send }: HandlerArgs) => {
+    const prisma = new PrismaClient();
+
+    const now = new Date();
+
+    const currentGame = await prisma.game.findFirst({ where: { opensAt: { lte: now }, endsAt: { gt: now } } });
+    if (!currentGame) {
+        return send(404, { message: 'No current game.' });
+    }
+
+    const clanStats: Stats[] = await prisma.$queryRaw`
+        SELECT
+            Clan.name AS clanName,
+            ClanMultiplier.value AS clanMultiplier,
+            COUNT(Nft.id) AS total,
+            COUNT(Quest.id) AS played,
+            SUM(
+                ROUND(
+                    MIN(
+                        (CAST(Game.endsAt AS FLOAT) - Quest.startedAt) / (CAST(Game.endsAt AS FLOAT) - Game.startsAt),
+                        1.0
+                    ) * ClanMultiplier.value * 100 + 0.5
+                )
+            ) AS points
+        FROM
+            Game
+            INNER JOIN ClanMultiplier ON ClanMultiplier.gameId = Game.id
+            INNER JOIN Clan ON Clan.id = ClanMultiplier.clanId
+            INNER JOIN Nft ON Nft.clanId = Clan.id
+            LEFT JOIN Quest ON Quest.gameId = Game.id AND Quest.nftId = Nft.id
+        WHERE
+            Game.id = ${currentGame.id}
+        GROUP BY
+            Clan.name
+    `;
+
+    const { endsAt, opensAt, startsAt } = currentGame;
+
+    send(200, {
+        clanStats: clanStats.reduce((result, { clanName, ...rest }) => ({ [clanName!]: rest, ...result }), {}),
+        endsAt,
+        opensAt,
+        startsAt,
+    } as GetCurrentGameResult);
+};
+
+export default handleJsonResponse(handler);
